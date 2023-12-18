749b581ae75dbe4aa573c3f59cfc7cb3941e8272
daquioag <aldaquioag@gmail.com>
2023-12-17 19:06:49 -0800

    create endpoint for creating users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,17 @@
-import { Controller, Post, Inject } from "@nestjs/common";
+import { Controller, Post, Inject, Body } from "@nestjs/common";
 import { ClientProxy } from "@nestjs/microservices";
+import { CreateUserDto } from "./dtos/CreateUser.dto";
 
 @Controller('users')
 export class UsersController {
+    constructor(@Inject('NATS_SERVICE') private natsclient: ClientProxy){}
 
     // not interacting with a database in this controller
     // but interact with the NATS service
     // need to inject NATS service in this class
-
-    constructor(@Inject('NATS_SERVICE') private natscliend: ClientProxy){
-
-    }
-    @Post()
-    createUser() {
-
+    @Post('create')
+    createUser(@Body()createUserDto: CreateUserDto) {
+        console.log(createUserDto)
+        this.natsclient.send( {cmd: 'createUser'}, createUserDto)
     }
 }
